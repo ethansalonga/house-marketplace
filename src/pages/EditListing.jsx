4ab9c2e64cf7790920322c63372061da28ff0f28
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-05 14:12:32 -0400

    Clear up console warnings

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -14,6 +14,7 @@ import { v4 as uuidv4 } from "uuid"
 import Spinner from "../components/Spinner"
 
 function EditListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(false)
   const [loading, setLoading] = useState(false)
   const [listing, setListing] = useState(false)
@@ -96,6 +97,7 @@ function EditListing() {
     return () => {
       isMounted.current = false
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted])
 
   const onSubmit = async (e) => {
@@ -165,6 +167,8 @@ function EditListing() {
                 break
               case "running":
                 console.log("Upload is running")
+                break
+              default:
                 break
             }
           },
